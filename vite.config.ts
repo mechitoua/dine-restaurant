1d475d6d1d981bf02258861cec9f643eb2c69d9a
mehmet_emin <mechitoua.m.amine@outlook.sa>
2024-11-24 18:54:08 +0100

    🎉 feat: initial project setup
    - Initialize React + TypeScript project with Vite
    - Configure TypeScript compiler settings
    - Set up project structure and dependencies

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
+});
